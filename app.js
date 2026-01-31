c92c4d825b366e96134e16a216ddd8354f994eb4
Jeyaraman S <jeyaramanapr22@gmail.com>
2026-01-31 08:51:23 +0530

    Added a cors in app.js

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require("path");
 const dotenv=require('dotenv');
+const cors = require("cors");
 
 dotenv.config({path:path.join(__dirname,"config/config.env")});
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
